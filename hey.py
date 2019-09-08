68f3ebd1c2642887b363ec57a56dd238254531e4
Raghu Rajagopalan <raghu.rajagopalan@gmail.com>
2019-09-08 20:02:09 +0530

    working rev

--- hey.py
+++ hey.py
@@ -1,18 +1,24 @@
-#! env python3
+#!/usr/bin/env python3
 import sys
 import os
 import logging
 import requests
 from dateparser import parse
 import configparser
+import subprocess
 
-config = configparser.ConfigParser()
-config.read(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config'))
+SETTINGS = {"PREFER_DATES_FROM": 'future'}
+FORMAT = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+logging.basicConfig(format=FORMAT, level=logging.WARNING)
 
-BOT_TOKEN = config["default"]["BOT_TOKEN"]
-BOT_CHAT = config["default"]["BOT_CHAT"]
+def readConfig():
+    config = configparser.ConfigParser()
+    config_path1 = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'config')
+    config_path2 = "/etc/hey.conf"
+    config_path3 = os.path.expanduser("~/.config/hey.conf")
+    config.read([config_path1, config_path2, config_path3])
+    return config
 
-settings = {"PREFER_DATES_FROM": 'future'}
 
 def getMessageAndTime(args):
     if args[1] == '/t':
@@ -20,7 +26,7 @@ def getMessageAndTime(args):
         while args[argi] != "/m":
             whenstr = whenstr + " " + args[argi]
             argi = argi + 1
-        when = parse(whenstr, settings=settings)
+        when = parse(whenstr, settings=SETTINGS)
         message = " ".join(args[argi + 1:])
         return (when, message)
     message = " ".join(args[1:])
@@ -31,13 +37,32 @@ def main(args):
     :returns: TODO
 
     """
+    config = readConfig()
+    BOT_TOKEN = config["default"]["BOT_TOKEN"]
+    BOT_CHAT = config["default"]["BOT_CHAT"]
     when, message = getMessageAndTime(args)
-    print(when, message)
+    logging.debug(when, message)
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage?chat_id={BOT_CHAT}&text={message}"
-    print(url)
+    logging.debug(url)
     if not when:
         r = requests.post(url)
-        print(r.text)
+        logging.debug(r.text)
+        return 0 # fix this based on http status
+    else:
+        timestr = when.strftime("%I:%M %p %Y-%m-%d")
+        status = subprocess.run(["at", timestr],
+                                input=f"{os.path.abspath(__file__)} {message}",
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                encoding="utf8")
+        if status.returncode == 0:
+            print(f"reminder '{message}' set for {timestr}")
+            return 0
+        else:
+            print("Setting reminder with at failed. Here's the output of at")
+            print(status.stdout)
+            print(status.stderr)
+            return 1
 
 if __name__ == "__main__":
-    main(sys.argv)
+    sys.exit(main(sys.argv))
